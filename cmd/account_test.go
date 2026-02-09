591957a176cdee4ee2c23cfa295295c56bf24944
Steve Cook <stevenbradleycook@gmail.com>
2026-02-09 13:08:13 -0500

    Update account tests

--- cmd/account_test.go
+++ cmd/account_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 // TestAuthCommand tests the basic structure of the account command
@@ -14,14 +13,14 @@ func TestAccountCommand(t *testing.T) {
 	assert.NotEmpty(t, accountCmd.Long)
 }
 
-// TestAccountCommandHasSubcommands verifies that show are registered
-func TestAuthCommandHasSubcommands(t *testing.T) {
+// TestAccountCommandHasSubcommands verifies that show subcommand is registered
+func TestAccountCommandHasSubcommands(t *testing.T) {
 	commands := accountCmd.Commands()
 
-	// Should have at least 2 subcommands (login and logout)
-	assert.GreaterOrEqual(t, len(commands), 2)
+	// Should have at least 1 subcommand (show)
+	assert.GreaterOrEqual(t, len(commands), 1)
 
-	// Find login and logout commands
+	// Find show command
 	var hasShow bool
 	for _, cmd := range commands {
 		if cmd.Use == "show" {
